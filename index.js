5472f01d2e25a9fe8bda252704eb9ff9331cb95b
alexandr-coderua <61251227+alexandr-coderua@users.noreply.github.com>
2020-09-08 18:19:01 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -92,11 +92,7 @@ app.get('/token/api?:t', function(req, res) {
 				if(response.statusCode == 200){
 					if(response.body.includes('Rejected') == false){
 						var response = JSON.parse(response.body);
-					}else{
-						var live = true;
-						res.json({balance: balance, stickers: stickers, live: live});	
 					}
-					if (error){};
 					if(response['error'] != undefined || response['results'] == undefined){
 						var live = false;
 						res.json({balance: balance, stickers: stickers, live: live});					
